7d0344dc7c3baa6e5b28ea6a8b8e9e5cf1061307
mbreza <breza.mateusz@gmail.com>
2018-01-12 23:20:07 +0100

    creating test for dodaj_mrowki, finding error in this method

---
+++ spec/ants_spec.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+require_relative '../lib/ants'
+require_relative '../lib/board'
+
+describe Ants do 
+   context "When calling dodaj_mrowki and given szerokosc, plansza, mrowki" do 
+      
+      it "should add ants to board and return Array" do 
+         a = Ants.new 
+         b = Board.new
+         licznik = 0
+         wysokosc = 50
+         szerokosc = 100
+         mrowki = 60
+         plansza = b.wypelnij_plansze(wysokosc, szerokosc)
+         expect(a.dodaj_mrowki szerokosc, plansza, mrowki).to be_a Array
+          
+         for i in 0..wysokosc-1
+            for j in 0..szerokosc-1
+                if plansza[i][j] == "m" then
+                    licznik += 1
+                    puts licznik
+                end
+            end
+        end 
+             
+       expect(licznik).to eql mrowki
+      end 
+      
+   end 
+end
