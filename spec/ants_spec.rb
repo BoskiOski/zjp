039a4350f95f6aa5b92ec175173baa65b7708d11
vvisnia <ziomek131212@wp.pl>
2018-01-14 02:49:08 +0100

    Test for ants movement

--- spec/ants_spec.rb
+++ spec/ants_spec.rb
@@ -2,11 +2,32 @@
 require_relative '../lib/ants'
 require_relative '../lib/board'
 
-describe Ants do 
-   context "When calling dodaj_mrowki and given wymiary, plansza, mrowki" do 
-      
-      it "should add ants to board and return Array" do 
-         a = Ants.new 
+
+
+describe Ants do
+  describe "ants_test" do
+  
+     describe "When calling mrowki_sie_poruszaja" do
+      it "ants moved" do
+         a = Ants.new
+         b = Board.new
+         wysokosc = 20
+         szerokosc = 40
+         wymiary = [20,40]
+         mrowki = 200
+          plansza = b.wypelnij_plansze(wysokosc, szerokosc)
+          plansza = a.dodaj_mrowki(wymiary, plansza, mrowki)
+          plansza_temp = Marshal.load(Marshal.dump(plansza))
+          plansza = a.mrowki_sie_poruszaja(wymiary, plansza)        
+          expect(plansza).to_not eq (plansza_temp)
+          expect(a.mrowki_sie_poruszaja wymiary, plansza).to be_a Array
+      end
+    end
+     
+     
+    describe "When calling dodaj_mrowki and given wymiary, plansza, mrowki" do
+      it "should add ants to board and return Array" do
+        a = Ants.new
          b = Board.new
          licznik = 0
          wysokosc = 50
@@ -15,16 +36,18 @@ describe Ants do
          mrowki = 60
          plansza = b.wypelnij_plansze(wysokosc, szerokosc)
          expect(a.dodaj_mrowki wymiary, plansza, mrowki).to be_a Array
-          
+         
          for i in 0..wysokosc-1
             for j in 0..szerokosc-1
                 if plansza[i][j] == "m" then
                     licznik += 1
                 end
             end
-        end    
+        end   
        expect(licznik).to eql mrowki
-      end 
-      
-   end 
+      end
+    end      
+       
+  end
 end
+
