f86e07733c937ff76b6fd07c66732658c98abff6
vvisnia <ziomek131212@wp.pl>
2018-01-14 06:13:56 +0100

    Clean board smell

--- lib/board.rb
+++ lib/board.rb
@@ -2,36 +2,41 @@
 
 #Class responsible for creating and printing board.
 class Board
-	
-    def wypelnij_plansze(wysokosc, szerokosc)
-        plansza = Array.new(wysokosc) {Array.new(szerokosc)}   
+   
+    def initialize
+        @plansza = Array.new()
+    end
+   
+    def wypelnij_plansze(wysokosc, szerokosc) 
         for i in 0..wysokosc-1
+            @plansza[i] = Array.new()
             for j in 0..szerokosc-1
-                plansza[i][j] = " "
+                @plansza[i][j] = " "
             end
         end
-        return plansza
+       
+        return @plansza
     end
-    
+   
     def rysuj_ramka(szerokosc)
-       ramka = "-"  
+       ramka = "-" 
        for i in 0..szerokosc
             ramka = ramka + "-"
-        end 
+        end
         puts ramka
     end
-    
-    def rysuj_plansza(wymiary, plansza)       
+   
+    def rysuj_plansza(wymiary, plansza)      
         rysuj_ramka(wymiary[1])
-        
-   	    for i in (0..wymiary[0]-1)
-			print "|"
-			for j in (0..wymiary[1]-1)
-				print plansza[i][j]
-			end
-			print "|\n"
-		end
-        
+       
+           for i in (0..wymiary[0]-1)
+            print "|"
+            for j in (0..wymiary[1]-1)
+                print plansza[i][j]
+            end
+            print "|\n"
+        end
+       
         rysuj_ramka(wymiary[1])
     end
 end
