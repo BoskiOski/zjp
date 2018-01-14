92453559f0d942af3c80683ad5981b5da80fb7cf
vvisnia <ziomek131212@wp.pl>
2018-01-14 06:21:15 +0100

    Test for filing the board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,7 +2,8 @@
 require_relative '../lib/board'
 
 describe Board do 
-   context "When calling rysuj_plansza and given wymiary, plansza" do 
+    describe "Board tests" do
+   describe "When calling rysuj_plansza and given wymiary, plansza" do 
       
       it "should print board" do 
          b = Board.new
@@ -14,4 +15,24 @@ describe Board do
       end 
       
    end 
+    
+    
+    describe "When calling wypelnij_plansze and given wysokosc, szerokosc" do 
+      
+      it "should fill board" do 
+         b = Board.new
+         wysokosc = 2
+         szerokosc = 3
+         wymiary = [2,3]
+         plansza = b.wypelnij_plansze(wysokosc, szerokosc)
+        expect(b.wypelnij_plansze wysokosc, szerokosc).to be_a Array
+          expect(plansza.length).to eql 2
+          expect(plansza[1].length).to eql 3
+      end 
+      
+   end 
+    
+    
+    
+end
 end
